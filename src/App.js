e55572640e892949e3458f530020abc1f8d7ad10
Zeno França Junior <zfrancajunior@gmail.com>
2017-06-18 16:41:40 -0300

    OK layout base
    layout base OK migrar interfaces para react

--- src/App.js
+++ src/App.js
@@ -5,15 +5,35 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  <div className="mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
+  <header className="mdl-layout__header">
+    <div className="mdl-layout__header-row">
+      <div className="mdl-layout-spacer"></div>
+      <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
+        <label className="mdl-button mdl-js-button mdl-button--icon"
+               for="fixed-header-drawer-exp">
+          <i className="material-icons">search</i>
+        </label>
+        
       </div>
+    </div>
+  </header>
+  <div className="mdl-layout__drawer">
+    <span className="mdl-layout-title">Moustache GED</span>
+    <nav className="mdl-navigation">
+      <a className="mdl-navigation__link" href="">Link</a>
+      <a className="mdl-navigation__link" href="">Link</a>
+      <a className="mdl-navigation__link" href="">Link</a>
+      <a className="mdl-navigation__link" href="">Link</a>
+    </nav>
+  </div>
+  <main className="mdl-layout__content">
+    <div className="page-content">
+
+    </div>
+  </main>
+</div>
+
     );
   }
 }
